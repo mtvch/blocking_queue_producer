f7e959c959173ee42ba632d0e2a52564158719a4
Matvey Karpov <matveichk@gmail.com>
2024-06-24 20:02:45 +0700

    Added ex_doc dep

--- mix.exs
+++ mix.exs
@@ -7,7 +7,18 @@ defmodule BlockingQueueProducer.MixProject do
       version: "0.1.0",
       elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
+      source_url: "https://github.com/mtvch/blocking_queue_producer",
+      description: "GenStage producer with back pressure for push model",
+      package: package(),
       deps: deps()
+    ]
+  end
+
+    defp package do
+    [
+      maintainers: ["Matvey Karpov"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/mtvch/blocking_queue_producer"}
     ]
   end
 
@@ -21,6 +32,7 @@ defmodule BlockingQueueProducer.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
       {:gen_stage, "~> 1.0"},
       {:telemetry, "~> 1.2"},
       {:prom_ex, "~> 1.9"}
