e25a875bdfc0fe5a20432cb601e9b3c26b6212c1
Matvey Karpov <matveichk@gmail.com>
2024-06-24 14:58:42 +0700

    Added PromEx plugin

---
+++ lib/blocking_queue_producer/prom_ex_plugin.ex
@@ -0,0 +1,67 @@
+defmodule BlockingQueueProducer.PromExPlugin do
+  @moduledoc """
+  Prometheus metrics for BlockingQueueProducer
+  """
+  use PromEx.Plugin
+
+  @impl true
+  def event_metrics(opts) do
+    otp_app = Keyword.fetch!(opts, :otp_app)
+
+    metric_prefix =
+      Keyword.get(opts, :metric_prefix, PromEx.metric_prefix(otp_app, :blocking_queue_producer))
+
+    Event.build(
+      :blocking_queue_producer_events_metrics,
+      [
+        sum(
+          metric_prefix ++ [:blocking_queue_producer, :events, :dispatched, :sum],
+          event_name: [:blocking_queue_producer, :events, :dispatched],
+          description: "Total number of dispatched events",
+          measurement: :count,
+          tags: [:name, :when]
+        )
+      ]
+    )
+  end
+
+  @impl true
+  def polling_metrics(opts) do
+    otp_app = Keyword.fetch!(opts, :otp_app)
+
+    metric_prefix =
+      Keyword.get(opts, :metric_prefix, PromEx.metric_prefix(otp_app, :blocking_queue_producer))
+
+    poll_rate = Keyword.get(opts, :poll_rate, 5_000)
+
+    blocking_queue_producers = Keyword.fetch!(opts, :blocking_queue_producers)
+
+    # Queue length details
+    Polling.build(
+      :blocking_queue_producer_polling_metrics,
+      poll_rate,
+      {__MODULE__, :execute_polling_metrics, [blocking_queue_producers]},
+      [
+        last_value(
+          metric_prefix ++ [:blocking_queue_producer, :queue, :length, :count],
+          event_name: [:blocking_queue_producer, :queue, :length],
+          description: "Number of undispatched events in a queue",
+          measurement: :count,
+          tags: [:name]
+        )
+      ]
+    )
+  end
+
+  def execute_polling_metrics(blocking_queue_producers)
+      when is_function(blocking_queue_producers) do
+    blocking_queue_producers.()
+    |> Enum.each(fn server ->
+      queue_length = BlockingQueueProducer.queue_length(server)
+
+      :telemetry.execute([:blocking_queue_producer, :queue, :length], %{count: queue_length}, %{
+        name: to_string(server)
+      })
+    end)
+  end
+end
